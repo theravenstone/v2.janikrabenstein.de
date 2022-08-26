8365c94366ae0c49167ca0f8ff16edd2a9dc266a
theravenstone <theravenstonepsn@gmail.com>
2022-08-26 18:15:28 +0200

    added tailwind

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -2,5 +2,9 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    css: [
+        '@/assets/css/tailwind.css',
+    ],
+    modules: ['@nuxtjs/tailwindcss']
+      
 })
--- website/pages/index.vue
+++ website/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  
+  <div class="">
+    test
+  </div>
 </template>
 
 <script>
