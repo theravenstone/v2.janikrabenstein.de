0b07dc3c1322a2c1dca560a97f7e9fa9ed749f3f
theravenstone <theravenstonepsn@gmail.com>
2022-09-11 13:55:21 +0200

    .

--- website/components/Tags.vue
+++ website/components/Tags.vue
@@ -1,15 +1,15 @@
 <template>
-  
-        <div class="flex justify-center">
-            <NuxtLink v-for="tag in tags" :key="tag" :to="'/blog/tags/' + tag.attributes.url_slug"
-                class="px-3 py-1 text-xs font-medium text-slate-100 transition-colors duration-300 transform bg-primary rounded-full cursor-pointer hover:bg-primary-hover m-2">
 
-                {{tag.attributes.name}}
+    <div class="flex justify-center">
+        <NuxtLink v-for="tag in tags" :key="tag" :to="'/blog/tags/' + tag.attributes.url_slug"
+            class="px-3 py-1 text-xs font-medium text-slate-100 transition-colors duration-300 transform bg-primary rounded-full cursor-pointer hover:bg-primary-hover m-2">
 
-            </NuxtLink>
-        </div>
+            {{tag.attributes.name}}
 
-   
+        </NuxtLink>
+    </div>
+
+
 </template>
 
 <script>
