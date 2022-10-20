59472c1c63d6c7242db1a2df753dcb4344446500
theravenstone <theravenstonepsn@gmail.com>
2022-10-20 19:52:43 +0200

    .

--- components/LatestPosts.vue
+++ components/LatestPosts.vue
@@ -1,60 +1,53 @@
 <template>
-  <section class="container mx-auto p-6 mb-12">
-    <div v-if="posts">
-      <span class="preheader">Blog</span>
-      <h2>Letzte Beiträge</h2>
-      <div class="mb-6">
-        <span class="inline-block w-40 h-1 mx-1 bg-primary rounded-full"></span>
-        <span class="inline-block w-5 h-1 mx-1 bg-primary rounded-full"></span>
-        <span class="inline-block w-1 h-1 mx-1 bg-primary rounded-full"></span>
-      </div>
+  <section v-if="posts" class="container mx-auto p-6 mb-12">
 
-      <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
-        <NuxtLink :to="'/blog/' + post.attributes.url_slug" v-for="post in posts" :key="post" class="group">
+    <span class="preheader">Blog</span>
+    <h2>Letzte Beiträge</h2>
+    <div class="mb-6">
+      <span class="inline-block w-40 h-1 mx-1 bg-primary rounded-full"></span>
+      <span class="inline-block w-5 h-1 mx-1 bg-primary rounded-full"></span>
+      <span class="inline-block w-1 h-1 mx-1 bg-primary rounded-full"></span>
+    </div>
 
-          <div class="overflow-hidden bg-gray-200 rounded-lg dark:bg-gray-800 h-full lg:hover:-translate-y-4 transition duration-300 relative flex flex-col flex-auto">
-            <img v-if="post.attributes.thumbnail.data" class="object-cover w-full h-48"
-              :src="'http://localhost:2345' + post.attributes.thumbnail.data.attributes.formats.medium.url"
-              :alt="post.attributes.thumbnail.data.attributes.alternativeText">
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
+      <NuxtLink :to="'/blog/' + post.attributes.url_slug" v-for="post in posts" :key="post" class="group">
 
-            <div class="p-6 h-full flex flex-col flex-auto">
-              <div>
+        <div
+          class="overflow-hidden bg-gray-200 rounded-lg dark:bg-gray-800 h-full lg:hover:-translate-y-4 transition duration-300 relative flex flex-col flex-auto">
+          <img v-if="post.attributes.thumbnail.data" class="object-cover w-full h-48"
+            :src="'http://localhost:2345' + post.attributes.thumbnail.data.attributes.formats.medium.url"
+            :alt="post.attributes.thumbnail.data.attributes.alternativeText">
 
-                <div class="mb-3">
-                  <NuxtLink v-for="tag in post.attributes.tags.data" :key="tag"
-                    :to="'/blog/tags/' + tag.attributes.url_slug"
-                    class="px-3 py-1 text-xs font-bold text-white transition-colors duration-300 transform border-2 border-primary bg-primary/10 rounded-full cursor-pointer hover:bg-primary mr-2 font-mono">
+          <div class="p-6 h-full flex flex-col flex-auto">
+            <div>
 
-                    {{tag.attributes.name}}
+              <div class="mb-3">
+                <NuxtLink v-for="tag in post.attributes.tags.data" :key="tag"
+                  :to="'/blog/tags/' + tag.attributes.url_slug"
+                  class="px-3 py-1 text-xs font-bold text-white transition-colors duration-300 transform border-2 border-primary bg-primary/10 rounded-full cursor-pointer hover:bg-primary mr-2 font-mono">
 
-                  </NuxtLink>
-                </div>
+                  {{tag.attributes.name}}
 
-                <h3>{{ post.attributes.title }}</h3>
-                <p class="text-gray-600 dark:text-gray-400">{{ post.attributes.teaser }}</p>
+                </NuxtLink>
               </div>
 
-              <div class="flex items-center justify-between mt-auto">
-                <NuxtLink :to="'/blog/' + post.attributes.url_slug" class="text-primary group-hover:text-primary-hover">
-                  Weiterlesen <i class="fa-solid fa-chevron-right my-auto ml-2 text-primary group-hover:text-primary-hover transition duration-300 lg:group-hover:translate-x-2"></i>
-                </NuxtLink>
+              <h3>{{ post.attributes.title }}</h3>
+              <p class="text-gray-600 dark:text-gray-400">{{ post.attributes.teaser }}</p>
+            </div>
 
-                <div class="sm:flex items-center hidden">
-                  <span class="text-gray-700 cursor-pointer dark:text-gray-200">Janik Rabenstein</span>
-                </div>
+            <div class="flex items-center justify-between mt-auto">
+              <NuxtLink :to="'/blog/' + post.attributes.url_slug" class="text-primary group-hover:text-primary-hover">
+                Weiterlesen <i
+                  class="fa-solid fa-chevron-right my-auto ml-2 text-primary group-hover:text-primary-hover transition duration-300 lg:group-hover:translate-x-2"></i>
+              </NuxtLink>
+
+              <div class="sm:flex items-center hidden">
+                <span class="text-gray-700 cursor-pointer dark:text-gray-200">Janik Rabenstein</span>
               </div>
             </div>
           </div>
-        </NuxtLink>
-      </div>
-
-
-    </div>
-    <div v-else>
-      <p>
-        <i class="fa-solid fa-triangle-exclamation mr-3 text-rose-500"></i>
-        <span>Es sind noch keine Blog Beiträge vorhanden.</span>
-      </p>
+        </div>
+      </NuxtLink>
     </div>
   </section>
 </template>
